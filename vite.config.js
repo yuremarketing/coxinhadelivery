885ee16fc487024aba08a79a0211583c6edeffa8
Seu Nome <seu-email@exemplo.com>
2026-01-17 11:41:25 -0300

    chore: configurando vite para detectar ambiente dev/prod automaticamente

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,25 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
-            refresh: true,
-        }),
-        react(),
-    ],
+export default defineConfig(({ command, mode }) => {
+    const isDevelopment = mode === 'development';
+
+    return {
+        server: {
+            // No seu PC (Sail), usa 0.0.0.0. No GitHub, usa o padrão.
+            host: isDevelopment ? '0.0.0.0' : 'localhost',
+            port: 5173,
+            strictPort: true,
+            hmr: {
+                host: 'localhost',
+            },
+        },
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.jsx'],
+                refresh: true,
+            }),
+            react(),
+        ],
+    };
 });
