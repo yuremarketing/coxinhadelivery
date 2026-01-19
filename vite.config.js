092b3c75a12bd9f8405ae2b19f99b8f476e6c91d
Seu Nome <seu-email@exemplo.com>
2026-01-19 01:14:26 -0300

    Fix: Configuração Vite e Views para .jsx

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        cors: true,
-        origin: 'http://localhost:5173',
-        hmr: {
-            host: 'localhost',
-        },
-    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx',
+            ],
             refresh: true,
         }),
         react(),
